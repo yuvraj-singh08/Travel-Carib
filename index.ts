c5332dd2c5aa88ca1e2c0b3ea6bbf978e28432c7
Atmajo <atmajoc@gmail.com>
2024-09-28 12:13:25 +0530

    Chore: added prisma to api /user/login, /user/register

--- index.ts
+++ index.ts
@@ -1,42 +1,53 @@
-import express from 'express'
-import cors from 'cors'
-import bodyParser from 'body-parser'
-import dotenv from 'dotenv';
-dotenv.config()
+import express, { NextFunction, Response } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+dotenv.config();
 
 //Router Imports
-import queryRoutes from './src/routes/queryRoutes';
-import kiuRoutes from './src/routes/kiuRoutes';
-import flightRoutes from './src/routes/flightRoutes';
-import duffelRoutes from './src/routes/duffelRoutes';
-import userRoutes from './src/routes/userRoutes';
-import { initMongo } from './config/mongo';
+import queryRoutes from "./src/routes/queryRoutes";
+import kiuRoutes from "./src/routes/kiuRoutes";
+import flightRoutes from "./src/routes/flightRoutes";
+import duffelRoutes from "./src/routes/duffelRoutes";
+import userRoutes from "./src/routes/userRoutes";
+import { initMongo } from "./config/mongo";
+import { AuthenticatedRequest } from "./types/express";
 
-const app = express()
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({
-    origin: '*', // Allow all origins
-}));
+app.use(
+  cors({
+    origin: "*", // Allow all origins
+  })
+);
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('The server is working fine and running on port 8000'); // Replace with your desired response
+app.get("/", (req, res) => {
+  res.send("The server is working fine and running on port 8000"); // Replace with your desired response
 });
-app.use('/user', userRoutes);
-app.use('/', queryRoutes)
-app.use('/kiu', kiuRoutes);
-app.use('/flight', flightRoutes);
-app.use('/duffel', duffelRoutes);
+app.use("/user", userRoutes);
+app.use("/", queryRoutes);
+app.use("/kiu", kiuRoutes);
+app.use("/flight", flightRoutes);
+app.use("/duffel", duffelRoutes);
+app.use(
+  (err: any, req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
 
+    res.status(statusCode).json({
+      success: false,
+      message: message,
+    });
+  }
+);
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`> App running on port ${PORT}  ...`);
-    try {
-        initMongo();
-    } catch (error) {
-        
-    }
+  console.log(`> App running on port ${PORT}  ...`);
+  try {
+    initMongo();
+  } catch (error) {}
 });
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router } from "express";
 import {
   updateUserProfile,
   getAllUsers,
@@ -7,28 +7,36 @@ import {
   deleteUser,
   registerUser,
   loginUser,
-} from '../controllers/userController';
-import {authenticateToken}  from '../middleware/authmiddleware';
-import { addPassenger, deletePassenger, updatePassenger } from '../controllers/passengerController';
-import { addCoTraveller, AddFrequentFlyer, UpdatePassportDetail } from '../controllers/CoTravellerController';
+} from "../controllers/userController";
+import { authenticateToken } from "../middleware/authmiddleware";
+import {
+  addPassenger,
+  deletePassenger,
+  updatePassenger,
+} from "../controllers/passengerController";
+import {
+  addCoTraveller,
+  AddFrequentFlyer,
+  UpdatePassportDetail,
+} from "../controllers/CoTravellerController";
 
 const router = Router();
-router.post('/register', registerUser);  
-router.post('/login', loginUser);        
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 // Protected Routes
-router.get('/profile', authenticateToken,updateUserProfile);
-router.put('/profile', authenticateToken, updateUserProfile);
-router.post('/traveller',authenticateToken,addCoTraveller);
-router.post('/passport', authenticateToken,UpdatePassportDetail);
-router.post('/flyer',authenticateToken, AddFrequentFlyer);
-router.get('/',  getAllUsers);
-router.get('/:id', authenticateToken, getUserById);
+router.get("/profile", authenticateToken, updateUserProfile);
+router.put("/profile", authenticateToken, updateUserProfile);
+router.post("/traveller", authenticateToken, addCoTraveller);
+router.post("/passport", authenticateToken, UpdatePassportDetail);
+router.post("/flyer", authenticateToken, AddFrequentFlyer);
+router.get("/", getAllUsers);
+router.get("/:id", authenticateToken, getUserById);
 
 // router.put('/:id', authenticateToken, updateUser);
-router.delete('/:id', authenticateToken, deleteUser);
-router.post('/addPassenger', authenticateToken,addPassenger);
-router.delete('/deletePassenger/:id',authenticateToken, deletePassenger);
-router.put('/updatePassenger/:id',authenticateToken, updatePassenger);
+router.delete("/:id", authenticateToken, deleteUser);
+router.post("/addPassenger", authenticateToken, addPassenger);
+router.delete("/deletePassenger/:id", authenticateToken, deletePassenger);
+router.put("/updatePassenger/:id", authenticateToken, updatePassenger);
 
 export default router;
