da9e7b11cc62991473094fce42c09317e65e1213
Suman Mondal <mondalsumanrock20@gmail.com>
2024-10-04 21:30:22 +0530

    changes on aws

--- src/utils/bucket.ts
+++ src/utils/bucket.ts
@@ -1,5 +1,6 @@
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import crypto from "crypto";
 
 const s3Client = new S3Client({
   region: process.env.AWS_REGION_NAME,
@@ -9,9 +10,19 @@ const s3Client = new S3Client({
   },
 });
 
+// Function to generate a random alphanumeric ID of length 5
+const generateRandomId = (length: number) => {
+  return crypto
+    .randomBytes(length)
+    .toString("base64")
+    .replace(/[^a-zA-Z0-9]/g, "")
+    .substring(0, length);
+};
+
 export const generateUploadUrl = async () => {
   const date = new Date();
-  const imageName = `vuelitos-${date.getTime()}.jpeg`;
+  const randomId = generateRandomId(5);
+  const imageName = `vuelitos-${randomId}.jpeg`;
 
   const command = new PutObjectCommand({
     Bucket: process.env.AWS_BUCKET_NAME as string,
@@ -20,7 +31,9 @@ export const generateUploadUrl = async () => {
   });
 
   try {
-    const signedUrl = await getSignedUrl(s3Client, command, { expiresIn: 1000 });
+    const signedUrl = await getSignedUrl(s3Client, command, {
+      expiresIn: 1000,
+    });
     return signedUrl;
   } catch (error) {
     console.error("Error generating signed URL:", error);
