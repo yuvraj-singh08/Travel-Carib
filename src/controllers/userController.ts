8ad7dbc6ca9bcbab87b82e918dae332673195ae1
Atmajo <atmajoc@gmail.com>
2024-10-14 23:10:07 +0530

    Refactor userController.ts to improve user authentication logic

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -66,12 +66,12 @@ export const registerUser = async (req: Request, res: Response) => {
 //Social Auth register
 export const socialAuthRegister = async (req: Request, res: Response) => {
   const data = req.body;
-  
+
   try {
     const response = await prisma.user.create({
       data: data,
     });
-    
+
     res.status(200).json({
       message: "User created successfully",
       user: response,
@@ -98,10 +98,14 @@ export const loginUser = async (req: Request, res: Response) => {
       return res.status(404).json({ error: "User not found" });
     }
 
-    const isPasswordValid = await bcrypt.compare(password, user.password);
+    let isPasswordValid: Boolean;
 
-    if (!isPasswordValid) {
-      return res.status(401).json({ error: "Invalid credentials" });
+    if (password) {
+      isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordValid) {
+        return res.status(401).json({ error: "Invalid credentials" });
+      }
     }
 
     const token = jwt.sign({ userId: user.id }, JWT_SECRET, {
