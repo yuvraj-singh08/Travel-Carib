3efa25cb06b600c25f3ed9e303e0ae953d260826
hemant9808 <143936287+Hemant9808@users.noreply.github.com>
2025-04-21 12:45:02 +0530

    update multer config

--- src/utils/multer.ts
+++ src/utils/multer.ts
@@ -1,10 +1,7 @@
 import multer from "multer";
 
-// const storage = multer.memoryStorage(); // store file in memory buffer
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, 'uploads/'), // Make sure 'uploads' folder exists
-    filename: (req, file, cb) => cb(null, Date.now() + '-' + file.originalname)
-  });
+const storage = multer.memoryStorage(); // store file in memory buffer
+
   
 
 export const upload = multer({ storage });
